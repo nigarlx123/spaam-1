ef20fbfb7678c0dc2849d67fdce708a3cd8c9ac7
nigarlx123 <47158913+nigarlx123@users.noreply.github.com>
2019-02-07 16:28:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("543088035860971560")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`Niga Make Credits`);
 }, 30)
 })
 
